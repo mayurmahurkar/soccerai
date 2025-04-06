98674821312485449623f1c96b18327ba95c305d
mayurmahurkar <mpmahurkar@gmail.com>
2025-04-06 19:09:21 +0530

    modified clustering script to take path from config file

--- clustering.py
+++ clustering.py
@@ -2,10 +2,15 @@ import supervision as sv
 from tqdm import tqdm
 from scripts.team import TeamClassifier
 from ultralytics import YOLO
+from scripts.utils import load_config
 
-SOURCE_VIDEO_PATH = "input/121364_0.mp4"
-PLAYER_DETECTION_MODEL = YOLO("models/player_detect/weights/best.pt")
-PLAYER_ID = 2
+# Load configuration
+config = load_config()
+
+# Use configuration values
+SOURCE_VIDEO_PATH = config["SOURCE_VIDEO_PATH"]
+PLAYER_DETECTION_MODEL = YOLO(config["PLAYER_DETECTION_MODEL"])
+PLAYER_ID = config["PLAYER_ID"]
 STRIDE = 30
 
 frame_generator = sv.get_video_frames_generator(
@@ -19,6 +24,6 @@ for frame in tqdm(frame_generator, desc='collecting crops'):
     players_crops = [sv.crop_image(frame, xyxy) for xyxy in detections.xyxy]
     crops += players_crops
 
-team_classifier = TeamClassifier(device="cuda")
+team_classifier = TeamClassifier(device=config["DEVICE"])
 team_classifier.fit(crops)
-team_classifier.save("models/team_classifier.pkl")
+team_classifier.save(config["TEAM_CLASSIFIER_MODEL"])
